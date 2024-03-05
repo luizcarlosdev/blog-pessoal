edf0115e591eed512770f771935d0214575f27c6
Luiz Carlos <luiz.carlosdev@outlook.com>
2024-03-05 08:31:11 -0300

    adição de menu lateral

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    const navigation = document.querySelector("#navigation");
+    navigation.style.display = "block"
+}
+
+function closeMenu() {
+    const navigation = document.querySelector("#navigation");
+    navigation.style.display = "none"
+}
