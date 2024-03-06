ced5dc3574d5cb5696238d3aa6f230f75befb48c
Luiz Carlos <luiz.carlosdev@outlook.com>
2024-03-06 08:13:38 -0300

    inicio de modificações no menu e adições de funcionalidades em Javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,3 +7,12 @@ function closeMenu() {
     const navigation = document.querySelector("#navigation");
     navigation.style.display = "none"
 }
+
+let nav_list_disable = [...document.querySelectorAll(".nav-list-disable")];
+
+nav_list_disable.map((elementosNav) => {
+    elementosNav.addEventListener("click",(event) => {
+        event.preventDefault()
+        console.log("ok")
+    })
+})
