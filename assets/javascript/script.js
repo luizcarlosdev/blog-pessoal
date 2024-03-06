3e088c20ea07912801ced561c2014fc02900f7ec
Luiz Carlos <luiz.carlosdev@outlook.com>
2024-03-06 09:18:46 -0300

    menu chavose : )

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,13 +6,45 @@ function openMenu() {
 function closeMenu() {
     const navigation = document.querySelector("#navigation");
     navigation.style.display = "none"
-}
+};
 
 let nav_list_disable = [...document.querySelectorAll(".nav-list-disable")];
 
 nav_list_disable.map((elementosNav) => {
-    elementosNav.addEventListener("click",(event) => {
-        event.preventDefault()
-        console.log("ok")
+    elementosNav.addEventListener("click", (event) => {
+        event.preventDefault();
     })
-})
+});
+
+const openSettings = document.querySelector("#OpenSettings");
+openSettings.addEventListener("click", () => {
+    const nav_list_1 = document.querySelector("#nav-options-list1");
+    openSettings.classList.toggle("openandclose1");
+    if (openSettings.classList.contains("openandclose1")) {
+        nav_list_1.style.display = "block"
+    } else {
+        nav_list_1.style.display = "none"
+    }
+});
+
+const openCreate = document.querySelector("#openCreate");
+openCreate.addEventListener("click", () => {
+    const nav_list_2 = document.querySelector("#nav-options-list2");
+    openCreate.classList.toggle("openandclose2");
+    if (openCreate.classList.contains("openandclose2")) {
+        nav_list_2.style.display = "block";
+    } else {
+        nav_list_2.style.display = "none";
+    }
+});
+
+const openUser = document.querySelector("#openAccount");
+openUser.addEventListener("click", () => {
+    const nav_list_3 = document.querySelector("#nav-options-list3");
+    openUser.classList.toggle("bah");
+    if (openUser.classList.contains("bah")) {
+        nav_list_3.style.display = "block";
+    } else {
+        nav_list_3.style.display = "none";
+    }
+});
