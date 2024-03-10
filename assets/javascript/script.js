47d58bdd23835356f142d0cf43853dd426fddd59
Luiz Carlos <luiz.carlosdev@outlook.com>
2024-03-09 22:07:03 -0300

    alt

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,34 +18,43 @@ nav_list_disable.map((elementosNav) => {
 
 const openSettings = document.querySelector("#OpenSettings");
 openSettings.addEventListener("click", () => {
+    const navMarker1 = document.querySelector("#nav-marker-1");
     const nav_list_1 = document.querySelector("#nav-options-list1");
     openSettings.classList.toggle("openandclose1");
     if (openSettings.classList.contains("openandclose1")) {
-        nav_list_1.style.display = "block"
+        nav_list_1.style.display = "block";
+        navMarker1.textContent = `\u2B9D`;
     } else {
-        nav_list_1.style.display = "none"
+        nav_list_1.style.display = "none";
+        navMarker1.textContent = `\u2B9F`;
     }
 });
 
 const openCreate = document.querySelector("#openCreate");
 openCreate.addEventListener("click", () => {
+    const navMarker2 = document.querySelector("#nav-marker-2");
     const nav_list_2 = document.querySelector("#nav-options-list2");
     openCreate.classList.toggle("openandclose2");
     if (openCreate.classList.contains("openandclose2")) {
         nav_list_2.style.display = "block";
+        navMarker2.textContent = `\u2B9D`;
     } else {
         nav_list_2.style.display = "none";
+        navMarker2.textContent = `\u2B9F`;
     }
 });
 
 const openUser = document.querySelector("#openAccount");
 openUser.addEventListener("click", () => {
+    const navMarker3 = document.querySelector("#nav-marker-3");
     const nav_list_3 = document.querySelector("#nav-options-list3");
     openUser.classList.toggle("bah");
     if (openUser.classList.contains("bah")) {
         nav_list_3.style.display = "block";
+        navMarker3.textContent = `\u2B9D`;
     } else {
         nav_list_3.style.display = "none";
+        navMarker3.textContent = `\u2B9F`;
     }
 });
 
@@ -63,6 +72,7 @@ function changeHeart() {
 let darkModeButton = document.querySelector("#dark-mode-btn");
 
 darkModeButton.addEventListener("click", () => {
+    document.querySelector("#navigation").style.display = "none";
     darkModeButton.classList.toggle("changeMode");
     let msg = document.querySelector("#msg-nav-list1");
     let icon = document.querySelector("#icon-modes");
@@ -80,7 +90,17 @@ darkModeButton.addEventListener("click", () => {
     let tittleH5 = [...document.querySelectorAll("article h5")];
     let tittleH6 = [...document.querySelectorAll("article h6")];
     let heart = [...document.querySelectorAll(".icon-heart")];
+    let imageDesc = [...document.querySelectorAll(".description-image")];
+    let articleLink = [...document.querySelectorAll("article a")];
+    let body = document.querySelector("body");
     if (darkModeButton.classList.contains("changeMode")) {
+        body.style.background = "#0d1117";
+        articleLink.map(el => {
+            el.style.color = "#eee"
+        })
+        imageDesc.map((el) => {
+            el.style.color = "#eee"
+        })
         heart.map((el) => {
             el.style.color = "#eee"
         })
@@ -122,6 +142,13 @@ darkModeButton.addEventListener("click", () => {
         icon.className = "fa-solid fa-sun";
         main.style.background = "#0d1117";
     } else {
+        body.style.background = "#ffffff";
+        articleLink.map(el => {
+            el.style.color = "#000000c0"
+        })
+        imageDesc.map((el) => {
+            el.style.color = "#444"
+        })
         heart.map((el) => {
             el.style.color = "#f00"
         })
@@ -161,6 +188,7 @@ darkModeButton.addEventListener("click", () => {
         })
         msg.textContent = "Dark Mode";
         icon.className = "fa-solid fa-moon";
-        main.style.background = "#fff";
+        main.style.background = "#ffffff";
     }
+    
 })
