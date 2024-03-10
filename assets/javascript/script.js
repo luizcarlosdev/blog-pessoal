4ad733cb46e0c8b35543e6656ca2e5ffd3a4e64a
Luiz Carlos <luiz.carlosdev@outlook.com>
2024-03-10 13:20:57 -0300

    alt

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -69,11 +69,20 @@ function changeHeart() {
     }
 }
 
-let darkModeButton = document.querySelector("#dark-mode-btn");
+const listDarkTheme = document.querySelector("#list-dark");
+listDarkTheme.addEventListener("click", () => {
+    const blockOptions = document.querySelector("#block-options");
+    listDarkTheme.classList.toggle("theme");
+    if (listDarkTheme.classList.contains("theme")) {
+        blockOptions.style.display = "block";
+    } else {
+        blockOptions.style.display = "none";
+    }
+})
 
-darkModeButton.addEventListener("click", () => {
-    document.querySelector("#navigation").style.display = "none";
-    darkModeButton.classList.toggle("changeMode");
+const blockDarkTheme = document.querySelector("#listAllDark");
+blockDarkTheme.addEventListener("click", () => {
+    blockDarkTheme.classList.toggle("blDark");
     let msg = document.querySelector("#msg-nav-list1");
     let icon = document.querySelector("#icon-modes");
     let main = document.querySelector("main");
@@ -92,8 +101,20 @@ darkModeButton.addEventListener("click", () => {
     let heart = [...document.querySelectorAll(".icon-heart")];
     let imageDesc = [...document.querySelectorAll(".description-image")];
     let articleLink = [...document.querySelectorAll("article a")];
+    let notationTittle = [...document.querySelectorAll(".notation h1")];
+    let notationParagraph = [...document.querySelectorAll(".notation p")];
+    let iconOptionsIcon = document.querySelector("#block-options-icon i");
+    let iconOptionsText = document.querySelector("#block-options-text");
     let body = document.querySelector("body");
-    if (darkModeButton.classList.contains("changeMode")) {
+    if (blockDarkTheme.classList.contains("blDark")) {
+        iconOptionsIcon.className = "fa-solid fa-sun";
+        iconOptionsText.textContent = "Light Mode";
+        notationParagraph.map(el => {
+            el.style.color = "#eee"
+        })
+        notationTittle.map(el => {
+            el.style.color = "#eee"
+        })
         body.style.background = "#0d1117";
         articleLink.map(el => {
             el.style.color = "#eee"
@@ -101,9 +122,9 @@ darkModeButton.addEventListener("click", () => {
         imageDesc.map((el) => {
             el.style.color = "#eee"
         })
-        heart.map((el) => {
-            el.style.color = "#eee"
-        })
+        // heart.map((el) => {
+        //     el.style.color = "#eee"
+        // })
         tittleH6.map((el) => {
             el.style.color = "#eee"
         })
@@ -142,6 +163,14 @@ darkModeButton.addEventListener("click", () => {
         icon.className = "fa-solid fa-sun";
         main.style.background = "#0d1117";
     } else {
+        iconOptionsIcon.className = "fa-solid fa-moon";
+        iconOptionsText.textContent = "Dark Mode";
+        notationParagraph.map(el => {
+            el.style.color = "#000"
+        })
+        notationTittle.map(el => {
+            el.style.color = "#000"
+        })
         body.style.background = "#ffffff";
         articleLink.map(el => {
             el.style.color = "#000000c0"
@@ -149,9 +178,9 @@ darkModeButton.addEventListener("click", () => {
         imageDesc.map((el) => {
             el.style.color = "#444"
         })
-        heart.map((el) => {
-            el.style.color = "#f00"
-        })
+        // heart.map((el) => {
+        //     el.style.color = "#f00"
+        // })
         tittleH6.map((el) => {
             el.style.color = "#000"
         })
@@ -190,5 +219,167 @@ darkModeButton.addEventListener("click", () => {
         icon.className = "fa-solid fa-moon";
         main.style.background = "#ffffff";
     }
-    
 })
+
+let darkModeButton = document.querySelector("#dark-mode-btn");
+
+darkModeButton.addEventListener("click", () => {
+    document.querySelector("#navigation").style.display = "none";
+    darkModeButton.classList.toggle("changeMode");
+    let msg = document.querySelector("#msg-nav-list1");
+    let icon = document.querySelector("#icon-modes");
+    let main = document.querySelector("main");
+    let card = [...document.querySelectorAll(".cards")];
+    let textCard = [...document.querySelectorAll("#text-card")];
+    let textCardH2 = document.querySelectorAll(".news a");
+    let th2 = [...textCardH2];
+    let footerCard = [...document.querySelectorAll("#quant-views")];
+    let articleParagraph = [...document.querySelectorAll("article p")];
+    let tittleH1 = [...document.querySelectorAll("article h1")];
+    let tittleH2 = [...document.querySelectorAll("article h2")];
+    let tittleH3 = [...document.querySelectorAll("article h3")];
+    let tittleH4 = [...document.querySelectorAll("article h4")];
+    let tittleH5 = [...document.querySelectorAll("article h5")];
+    let tittleH6 = [...document.querySelectorAll("article h6")];
+    let heart = [...document.querySelectorAll(".icon-heart")];
+    let imageDesc = [...document.querySelectorAll(".description-image")];
+    let articleLink = [...document.querySelectorAll("article a")];
+    let notationTittle = [...document.querySelectorAll(".notation h1")];
+    let notationParagraph = [...document.querySelectorAll(".notation p")];
+    let body = document.querySelector("body");
+    if (darkModeButton.classList.contains("changeMode")) {
+        notationParagraph.map(el => {
+            el.style.color = "#eee"
+        })
+        notationTittle.map(el => {
+            el.style.color = "#eee"
+        })
+        body.style.background = "#0d1117";
+        articleLink.map(el => {
+            el.style.color = "#eee"
+        })
+        imageDesc.map((el) => {
+            el.style.color = "#eee"
+        })
+        // heart.map((el) => {
+        //     el.style.color = "#eee"
+        // })
+        tittleH6.map((el) => {
+            el.style.color = "#eee"
+        })
+        tittleH5.map((el) => {
+            el.style.color = "#eee"
+        })
+        tittleH4.map((el) => {
+            el.style.color = "#eee"
+        })
+        tittleH3.map((el) => {
+            el.style.color = "#eee"
+        })
+        tittleH2.map((el) => {
+            el.style.color = "#eee"
+        })
+        tittleH1.map((el) => {
+            el.style.color = "#eee"
+        })
+        articleParagraph.map((el) => {
+            el.style.color = "#eee"
+        })
+        footerCard.map((el) => {
+            el.style.color = "#eee"
+        })
+        th2.map((element) => {
+            element.style.color = "#eee"
+        })
+        textCard.map((e) => {
+            e.style.color = "#eee"
+        })
+        card.map((el) => {
+            el.style.background = "#17181c";
+            el.style.color = "#eee";
+        })
+        msg.textContent = "Light Mode";
+        icon.className = "fa-solid fa-sun";
+        main.style.background = "#0d1117";
+    } else {
+        notationParagraph.map(el => {
+            el.style.color = "#000"
+        })
+        notationTittle.map(el => {
+            el.style.color = "#000"
+        })
+        body.style.background = "#ffffff";
+        articleLink.map(el => {
+            el.style.color = "#000000c0"
+        })
+        imageDesc.map((el) => {
+            el.style.color = "#444"
+        })
+        // heart.map((el) => {
+        //     el.style.color = "#f00"
+        // })
+        tittleH6.map((el) => {
+            el.style.color = "#000"
+        })
+        tittleH5.map((el) => {
+            el.style.color = "#000"
+        })
+        tittleH4.map((el) => {
+            el.style.color = "#000"
+        })
+        tittleH3.map((el) => {
+            el.style.color = "#000"
+        })
+        tittleH2.map((el) => {
+            el.style.color = "#000"
+        })
+        tittleH1.map((el) => {
+            el.style.color = "#000"
+        })
+        articleParagraph.map((el) => {
+            el.style.color = "#000"
+        })
+        footerCard.map((el) => {
+            el.style.color = "#444"
+        })
+        th2.map((element) => {
+            element.style.color = "#444"
+        })
+        textCard.map((e) => {
+            e.style.color = "#444"
+        })
+        card.map((el) => {
+            el.style.background = "#fff";
+            el.style.color = "#444";
+        })
+        msg.textContent = "Dark Mode";
+        icon.className = "fa-solid fa-moon";
+        main.style.background = "#ffffff";
+    }
+
+})
+
+let slides = document.querySelectorAll('.slide');
+let current = 0;
+
+function slider() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+    }
+    slides[current].classList.add('active');
+}
+
+function nextSlide() {
+    current = (current != slides.length - 1) ? current + 1 : 0;
+    slider();
+}
+
+function prevSlide() {
+    current = (current != 0) ? current - 1 : slides.length - 1;
+    slider();
+}
+
+document.getElementById('next').addEventListener('click', nextSlide);
+document.getElementById('prev').addEventListener('click', prevSlide);
+
+setInterval(nextSlide, 5000);
