927e45cbac1d9478074c3c4298ca0fece78b3149
Luiz Carlos <luiz.carlosdev@outlook.com>
2024-03-27 08:32:31 -0300

    alt

--- assets/javascript/create.js
+++ assets/javascript/create.js
@@ -1,18 +1,32 @@
-const inputPassword = document.querySelector("#input-password");
-const buttonSignUp = document.querySelector("#btn-sign-up");
+const inputPasswordSignUp = document.querySelector("#input-password");
+const buttonSignUpEye = document.querySelector("#btn-sign-up");
+const buttonSubmitSignUp = document.querySelector("#button-submit-signUp");
 
-buttonSignUp.addEventListener("click", event => {
+// buttonSubmitSignUp.addEventListener("click", event => {
+//     event.preventDefault()
+// })
+
+buttonSignUpEye.addEventListener("click", event => {
     event.preventDefault()
-})
+});
 
-buttonSignUp.addEventListener("click",() => {
-    buttonSignUp.classList.toggle("changeIconEye");
+buttonSignUpEye.addEventListener("click", () => {
+    buttonSignUpEye.classList.toggle("changeIconEye");
     const iconEyeSignUp = document.querySelector("#icons-eye-sig-up")
-    if(buttonSignUp.classList.contains("changeIconEye")) {
+    if (buttonSignUpEye.classList.contains("changeIconEye")) {
         iconEyeSignUp.className = "fa-regular fa-eye";
-        inputPassword.type = "text"
+        inputPasswordSignUp.type = "text"
     } else {
         iconEyeSignUp.className = "fa-regular fa-eye-slash";
-        inputPassword.type = "password"
+        inputPasswordSignUp.type = "password"
     }
+});
+
+buttonSubmitSignUp.addEventListener("click",() => {
+    const userLoginMain = document.querySelector("#user-login-main");
+    const userLoginMainName = document.querySelector("#user-login-main-name span");
+    const userLoginMainTime = document.querySelector("#user-login-main-time span");
+    const tittleH2NavaHeader = document.querySelector("#nav-header h2");
+    tittleH2NavaHeader.style.display = "none";
+    userLoginMain.style.display = "block"
 })
